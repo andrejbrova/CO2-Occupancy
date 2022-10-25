fbefadfa2db6fcb9710e966f3cdd094362fdf838
Andrej Brova <68370526+andrejbrova@users.noreply.github.com>
2022-10-25 11:36:28 +0200

    Update test_models.py

--- models/test_models.py
+++ models/test_models.py
@@ -1,70 +1,105 @@
-from pathlib import Path
+import pathlib
 import sys
-
-ROOT_DIR = Path(__file__).parents[1]
-sys.path.append(str(ROOT_DIR))
-sys.path.append(str(ROOT_DIR.parent) + '/Repos/datascience/') # Path to datamodel location
-
+from matplotlib import pyplot as plt
 from tensorflow import keras
+from keras import layers
 from keras.models import Sequential
 from keras.metrics import BinaryAccuracy
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
 
-from keras.layers import Activation
-from keras.layers import Dropout, Dense, Input
-from keras.layers.embeddings import Embedding
+from keras import backend as K
+from keras import regularizers
+from keras.models import Model
+from keras.layers import Activation, BatchNormalization, Concatenate
+from keras.layers import Dropout, Dense, Input, Reshape
+from keras.layers import Embedding
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 
-from utils import load_dataset, summarize_results
-from datamodels import datamodels as dm
+directory = pathlib.Path(__file__).parent
+sys.path.append(str(directory.parent) + '/Repos/datascience/') # Path to datamodel location
 
-# References
-# https://towardsdatascience.com/a-comprehensive-guide-to-convolutional-neural-networks-the-eli5-way-3bd2b1164a53
-
+from utils2 import load_dataset, summarize_results
+import datamodels as dm
 
 def main():
-    dataset = 'uci'
-    batch_size = 32
-    epochs = 50
-    repeats = 10
-    historical_co2 = True
-    embedding = False # Wont work here
-    feature_set = 'CO2'
+    BATCH_SIZE = 32
+    EPOCHS = 200
+    REPEATS = 10
+    lookback_horizon = 48
+    prediction_horizon = 1
     model_name = 'CNN'
-    shaped = False
 
-    models = {
-        'CNN': (dm.ConvolutionNetwork, layers_CNN),
-        'SRNN': (dm.RecurrentNetwork, layers_SRNN),
-        'LSTM': (dm.VanillaLSTM, layers_CNN),
-        'GRU': (dm.GRU, layers_GRU)
-    }
+    X_train, X_test_1, X_test_2, X_test_combined, y_train, y_test_1, y_test_2, y_test_combined = load_dataset()
 
-    for historical_co2 in [1, 5, 10, 15, 30]:
-        X_train, X_test_1, X_test_2, X_test_combined, y_train, y_test_1, y_test_2, y_test_combined = load_dataset(
-            dataset=dataset,
-            feature_set=feature_set,
-            historical_co2=historical_co2,
-            normalize=True,
-            embedding=embedding,
-            shaped=shaped
+    print(f'Training on {X_train.shape[0]} samples.')
+    print(f'Testing on {X_test_1.shape[0]} samples (Test1).')
+    print(f'Testing on {X_test_2.shape[0]} samples (Test2).')
+    print(f'input: {X_train.shape[-1]} features ({X_train.columns.tolist()}).')
+
+    scores_train = []
+    scores_test_1 = []
+    scores_test_2 = []
+    scores_test_combined = []
+    for run in range(REPEATS):
+        print('Run ' + str(run + 1))
+        model = build_model(X_train.shape[0], X_train.shape[1], 1, BATCH_SIZE, EPOCHS, model_name)
+        acc_train, acc_test_1, acc_test_2, acc_test_combined = run_model(X_train, X_test_1, X_test_2, X_test_combined, y_train, y_test_1, y_test_2, y_test_combined, model, lookback_horizon, prediction_horizon)
+        scores_train.append(acc_train)
+        scores_test_1.append(acc_test_1)
+        scores_test_2.append(acc_test_2)
+        scores_test_combined.append(acc_test_combined)
+    summarize_results(scores_train, scores_test_1, scores_test_2, scores_test_combined, model_name).to_csv(directory + '/' + model_name + '.csv')
+
+
+def build_model(n_timesteps, n_features, target_shape, batch_size, epochs, name):
+
+    def SRNN_build_model_shallow(input_shape: tuple, target_shape: tuple) -> keras.Model:
+        hidden_layer_size = 32
+        return keras.Sequential(
+            [
+                keras.layers.Input(shape=input_shape),
+                keras.layers.Dropout(0.2),
+                keras.layers.SimpleRNN(units=64),
+                keras.layers.Dropout(0.2),
+                keras.layers.Dense(units=hidden_layer_size, activation='relu'),
+                keras.layers.Dense(units=target_shape[0], activation='sigmoid')
+            ]
         )
 
-        scores_train = []
-        scores_test_1 = []
-        scores_test_2 = []
-        scores_test_combined = []
-        
-        for run in range(repeats):
-            print('Run: ' + str(run + 1) + ', Dataset: ' + dataset + ', Model: ' + model_name)
-            model = build_model(X_train.shape[0], X_train.shape[-1], 1, batch_size, epochs, models[model_name], model_name)
-            acc_train, acc_test_1, acc_test_2, acc_test_combined = run_model(X_train, X_test_1, X_test_2, X_test_combined, y_train, y_test_1, y_test_2, y_test_combined, model)
-            scores_train.append(acc_train)
-            scores_test_1.append(acc_test_1)
-            scores_test_2.append(acc_test_2)
-            scores_test_combined.append(acc_test_combined)
-        
-        summarize_results(scores_train, scores_test_1, scores_test_2, scores_test_combined, model_name, dataset, batch_size, epochs, repeats, embedding, feature_set, historical_co2)
+    def SRNN_build_model(input_shape: tuple, target_shape: tuple) -> keras.Model:
+        hidden_layer_size = 32
+        return keras.Sequential(
+            [
+                keras.layers.Input(shape=input_shape),
+                keras.layers.SimpleRNN(return_sequences=True, units=64),
+                keras.layers.SimpleRNN(return_sequences=True, units=64),
+                keras.layers.Dropout(0.2),
+                keras.layers.SimpleRNN(return_sequences=True, units=64),
+                keras.layers.SimpleRNN(return_sequences=True, units=64),
+                keras.layers.SimpleRNN(units=64),
+                keras.layers.Dropout(0.2),
+                #keras.layers.SimpleRNN(units=64),
+                keras.layers.Dense(units=hidden_layer_size, activation='relu'),
+                keras.layers.Dense(units=target_shape[0], activation='sigmoid')
+            ]
+        )
 
-def build_model(n_timesteps, n_features, target_shape, batch_size, epochs, model_type, name):
+    def CNN_build_model(input_shape: tuple, target_shape: tuple) -> keras.Model:
+        return keras.Sequential(
+            [
+                keras.layers.Input(input_shape),
+                keras.layers.Conv1D(filters=32, kernel_size=3, activation="relu"),
+                keras.layers.Conv1D(filters=32, kernel_size=3, activation="relu"),
+                keras.layers.Conv1D(filters=32, kernel_size=3, activation="relu"),
+                keras.layers.Dropout(0.2),
+                keras.layers.Conv1D(filters=32, kernel_size=3, activation="relu"),
+                keras.layers.Dense(units=32, activation="relu"),
+                keras.layers.Flatten(),
+                keras.layers.Dropout(0.2),
+                keras.layers.Dense(target_shape[0], activation='sigmoid')
+            ]
+        )
     
     def compile_model(model: keras.Model):
         optimizer = keras.optimizers.Adam()
@@ -72,7 +107,7 @@ def build_model(n_timesteps, n_features, target_shape, batch_size, epochs, model
 
     def train_model(model, x_train, y_train) -> keras.callbacks.History:
         callbacks_list = [
-            keras.callbacks.EarlyStopping(monitor='val_binary_accuracy', patience=10)
+            keras.callbacks.EarlyStopping(monitor='binary_accuracy', patience=10)
         ]
         return model.fit(
             x_train, y_train,
@@ -82,16 +117,45 @@ def build_model(n_timesteps, n_features, target_shape, batch_size, epochs, model
             callbacks=callbacks_list
         )
 
-    model = model_type[0](
+    """model = dm.RecurrentNetwork(
         name='SIMPLE ' + name,
         y_scaler_class=dm.processing.Normalizer,
         compile_function=compile_model,
-        build_function=model_type[1],
+        build_function=SRNN_build_model,
+        train_function=train_model)"""
+
+    model = dm.ConvolutionNetwork(
+        name='SIMPLE ' + name,
+        y_scaler_class=dm.processing.Normalizer,
+        compile_function=compile_model,
+        build_function=CNN_build_model,
         train_function=train_model)
 
     return model
 
-def run_model(X_train, X_test_1, X_test_2, X_test_combined, y_train, y_test_1, y_test_2, y_test_combined, model):
+def run_model(X_train, X_test_1, X_test_2, X_test_combined, y_train, y_test_1, y_test_2, y_test_combined, model, lookback_horizon, prediction_horizon):
+    x_scaler = dm.processing.Normalizer().fit(X_train)
+    #y_scaler = dm.processing.Normalizer().fit(y_train)
+
+    model.set_x_scaler(x_scaler)
+    #model.set_y_scaler(y_scaler) # Not necessary for binary classification
+
+    X_train, y_train = dm.processing.shape.get_windows(
+        lookback_horizon, X_train.to_numpy(), prediction_horizon, y_train.to_numpy()
+    )
+
+    X_test_1, y_test_1 = dm.processing.shape.get_windows(
+        lookback_horizon, X_test_1.to_numpy(), prediction_horizon, y_test_1.to_numpy(),
+    )
+
+    X_test_2, y_test_2 = dm.processing.shape.get_windows(
+        lookback_horizon, X_test_2.to_numpy(), prediction_horizon, y_test_2.to_numpy(),
+    )
+
+    X_test_combined, y_test_combined = dm.processing.shape.get_windows(
+        lookback_horizon, X_test_combined.to_numpy(), prediction_horizon, y_test_combined.to_numpy(),
+    )
+
     model.train(X_train, y_train)
 
     y_pred_train = model.predict(X_train)
@@ -99,74 +163,16 @@ def run_model(X_train, X_test_1, X_test_2, X_test_combined, y_train, y_test_1, y
     y_pred_test_2 = model.predict(X_test_2)
     y_pred_test_combined = model.predict(X_test_combined)
 
-    acc_train = BinaryAccuracy()(y_train, y_pred_train)
-    acc_test_1 = BinaryAccuracy()(y_test_1, y_pred_test_1)
-    acc_test_2 = BinaryAccuracy()(y_test_2, y_pred_test_2)
-    acc_test_combined = BinaryAccuracy()(y_test_combined, y_pred_test_combined)
+    evaluation = BinaryAccuracy()
+    acc_train = evaluation(y_train, y_pred_train)
+    evaluation.reset_states()
+    acc_test_1 = evaluation(y_test_1, y_pred_test_1)
+    evaluation.reset_states()
+    acc_test_2 = evaluation(y_test_2, y_pred_test_2)
+    evaluation.reset_states()
+    acc_test_combined = evaluation(y_test_combined, y_pred_test_combined)
 
     return acc_train, acc_test_1, acc_test_2, acc_test_combined
-
-# Layers
-
-def layers_CNN(input_shape: tuple, target_shape: tuple) -> keras.Model:
-    return keras.Sequential([
-        keras.layers.Input(input_shape),
-        keras.layers.Conv1D(filters=32, kernel_size=3, activation="relu"),
-        keras.layers.Conv1D(filters=32, kernel_size=3, activation="relu"),
-        keras.layers.Conv1D(filters=32, kernel_size=3, activation="relu"),
-        keras.layers.Dropout(0.2),
-        keras.layers.Conv1D(filters=32, kernel_size=3, activation="relu"),
-        keras.layers.Dense(units=32, activation="relu"),
-        keras.layers.Flatten(),
-        keras.layers.Dropout(0.2),
-        keras.layers.Dense(target_shape[0], activation='sigmoid')
-    ])
-
-def layers_SRNN(input_shape: tuple, target_shape: tuple) -> keras.Model:
-    hidden_layer_size = 32
-    return keras.Sequential(
-        [
-            keras.layers.Input(shape=input_shape),
-            keras.layers.SimpleRNN(return_sequences=True, units=64),
-            keras.layers.SimpleRNN(return_sequences=True, units=64),
-            keras.layers.Dropout(0.2),
-            keras.layers.SimpleRNN(return_sequences=True, units=64),
-            keras.layers.SimpleRNN(return_sequences=True, units=64),
-            keras.layers.SimpleRNN(units=64),
-            keras.layers.Dropout(0.2),
-            #keras.layers.SimpleRNN(units=64),
-            keras.layers.Dense(units=hidden_layer_size, activation='relu'),
-            keras.layers.Dense(units=target_shape[0], activation='sigmoid')
-        ]
-    )
-
-def layers_LSTM(input_shape: tuple, target_shape: tuple) -> keras.Model:
-    hidden_layer_size = 32
-    return keras.Sequential(
-        [
-            keras.Input(shape=input_shape),
-            keras.layers.Dropout(0.2),
-            keras.layers.LSTM(units=64),
-            keras.layers.Dropout(0.2),
-            keras.layers.Dense(units=hidden_layer_size, activation='relu'),
-            keras.layers.Dense(units=target_shape[0], activation='sigmoid')
-        ]
-    )
-
-def layers_GRU(input_shape: tuple, target_shape: tuple) -> keras.Model:
-    hidden_layer_size = 32
-    return keras.Sequential(
-        [
-            keras.Input(shape=input_shape),
-            keras.layers.GRU(return_sequences=True, units=64),
-            keras.layers.Dropout(0.2),
-            keras.layers.GRU(return_sequences=True, units=64),
-            keras.layers.GRU(units=64),
-            keras.layers.Dropout(0.2),
-            keras.layers.Dense(units=hidden_layer_size, activation='relu'),
-            keras.layers.Dense(units=target_shape[0])
-        ]
-        )
 
 if __name__ == '__main__':
     main()
